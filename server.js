8078ae973d0e32f558900dc3cc2e221d528cb175
suecollarte <suecollarte@gmail.com>
2022-08-31 19:00:16 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
+/* ---------------------- Modulos ----------------------*/
+const express = require('express');
+const path = require('path');
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
 
+/* ---------------------- Instancia de servidor ----------------------*/
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+/* ---------------------- Middlewares ---------------------- */
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/*---------------------- Base de datos ----------------------*/
+const DB_MENSAJES = [
+    { author: "Juan", text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", text: "¡Genial!" }
+]
+
+/* ---------------------- Rutas ----------------------*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+/* ---------------------- Servidor ----------------------*/
+const PORT = 3000;
+const server = httpServer.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en puerto http://localhost:${PORT}`);
+})
+
+/* ---------------------- WebSocket ----------------------*/
+io.on('connection', (socket)=>{
+    console.log(`Nuevo cliente conectado! ${socket.id}`);
+    socket.emit('from-server-mensajes', {DB_MENSAJES});
+
+    socket.on('from-client-mensaje', mensaje => {
+        DB_MENSAJES.push(mensaje);
+        io.sockets.emit('from-server-mensajes', {DB_MENSAJES});
+    });
+})
